2bd3bcb9acdcf55b4a00653feed0a64d155c84b7
anthonymjimenez <anthonyjim96@gmail.com>
2020-11-12 12:24:26 -0500

    stuck on undefined user

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ let entriesLink = document.getElementById("entries-link");
 let publicLinks = document.getElementById("other-links");
 let commentList = document.getElementById("comment-list");
 let commentForm = document.getElementById("comment-form");
-let myEntriesDiv = document.createElement('div')
+let myEntriesDiv = document.createElement("div");
 let config = {
   method: "POST",
   headers: { "content-type": "application/json", accept: "application/json" },
 };
-
 
 var currentUser;
 // function to create elements with 'element' as argument and string you want inside element as 2nd argument.
@@ -32,7 +31,7 @@ const createLi = createElements("li");
 
 //places THIS user's info data on page
 function renderUser(data) {
-  userInfoContainer.innerText = ""
+  userInfoContainer.innerText = "";
   let userName = createH1(data.username);
   let name = createH2(data.name);
   let age = createH2(`Age: ${data.age}`);
@@ -52,62 +51,56 @@ async function fetchEntriesComments(id) {
   return data.comments;
 }
 
-
-const makePublicFetch =(entryIdNum) =>{
-
+const makePublicFetch = (entryIdNum) => {
   const makePublicConfig = {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
-    body: JSON.stringify({public: 'true'})
-  }
-
+    body: JSON.stringify({ public: "true" }),
+  };
   fetch(`${url}entries/${entryIdNum}`, makePublicConfig)
-  .then(r=>r.json())
-  .then(console.log)
-}
-
-
+    .then((r) => r.json())
+    .then(console.log);
+};
 
 ///event listener conditionals for buttons on user's entry
-const userBtnEvents = (userBtns, id) =>{
-  const entryIdNum = id
+const userBtnEvents = (userBtns, id) => {
+  const entryIdNum = id;
   // debugger
-  userBtns.addEventListener('click', (e)=>{
-    e.preventDefault()
-    if (e.target.matches('.make-public')){
-      console.log("make public clicked")
+  userBtns.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.matches(".make-public")) {
+      console.log("make public clicked");
       ///make public patch fetch request function
-    } else if (e.target.matches('.edit-post')){
-      console.log("edit post clicked")
+    } else if (e.target.matches(".edit-post")) {
+      console.log("edit post clicked");
       ///edit post patch fetch request function
-    } else if (e.target.matches('.delete-post')){
-      console.log("delete button clicked")
+    } else if (e.target.matches(".delete-post")) {
+      console.log("delete button clicked");
       /// delete post delete fetch request function
     }
-  })
-}
-
+  });
+};
 
 //button showing conditional statement
 const thisUserEntry = (user_id, id) => {
-  const userBtns = document.querySelector('.user-btns')
-  if (sessionStorage.getItem('user_id') == user_id){
-    userBtns.style.display = ""
-    userBtnEvents(userBtns, id)
+  const userBtns = document.querySelector(".user-btns");
+  if (sessionStorage.getItem("user_id") == user_id) {
+    userBtns.style.display = "";
+    userBtnEvents(userBtns, id);
   } else {
-    userBtns.style.display = "none"
+    userBtns.style.display = "none";
   }
-}
+};
 
 // loads new post on THIS page when a different post is clicked
 async function renderEntry({ title, description, id, user_id }) {
   let { username, name } = await fetchUser(user_id);
   let comments = await fetchEntriesComments(id);
-  entryContainer.dataset.entryId = id
-  entryContainer.dataset.userId = user_id
+  entryContainer.dataset.entryId = id;
+  entryContainer.dataset.userId = user_id;
   let [titleNode, text, buttons] = entryContainer
     .querySelector(".card-body")
     .querySelectorAll(".node");
@@ -115,10 +108,10 @@ async function renderEntry({ title, description, id, user_id }) {
   text.innerText = description;
   //button add event to go public / edit post / delete post
   /// function name here ///
-  thisUserEntry(user_id, id)
+  thisUserEntry(user_id, id);
   // debugger
 
-  //create data set for entry - override 
+  //create data set for entry - override
   renderComments(comments);
 }
 
@@ -141,7 +134,7 @@ function renderEntriesLinks({ entries }) {
     entryLink.addEventListener("click", () => {
       renderEntry(entry);
     });
-    myEntriesDiv.appendChild(entryLink)
+    myEntriesDiv.appendChild(entryLink);
   });
   entriesLink.appendChild(myEntriesDiv);
 }
@@ -169,12 +162,12 @@ const fetchAllEntries = (callBackFunc) => {
 };
 
 const appendLinksToPage = (entries) => {
-  publicLinks.innerText = ""
-  const explore = document.createElement('h5')
-  explore.textContent = "Explore Public Post!"
+  publicLinks.innerText = "";
+  const explore = document.createElement("h5");
+  explore.textContent = "Explore Public Post!";
   const otherLinksUl = document.createElement("ul");
   // otherLinksUl.textContent = "Post by Other Users";
-  entries.forEach((entry) => {
+  entries.filter(filterPrivatePost).forEach((entry) => {
     const link = createLi(entry.title);
     link.addEventListener("click", () => renderEntry(entry));
     otherLinksUl.append(link);
@@ -182,17 +175,26 @@ const appendLinksToPage = (entries) => {
   publicLinks.append(explore, otherLinksUl);
 };
 
+const filterPrivatePost = ({ public, user_id }) => public == true; // && user_id != sessionStorage.getItem('user_id')
+
 const addComment = ({ id }) => {
   // debugger
   commentForm.addEventListener("submit", async (e) => {
-    if (id === sessionStorage.getItem('user_id')){
+    if (id === sessionStorage.getItem("user_id")) {
       // debugger
-        // query for data set connected to current entry
+      // query for data set connected to current entry
 
       e.preventDefault();
 
-      const entryConID = parseInt(entryContainer.dataset.entryId)
-      let post = await fetch(`${url}comments`, {...config, body: JSON.stringify({comment: document.getElementById('comment').value, user_id: id, entry_id: entryConID})});
+      const entryConID = parseInt(entryContainer.dataset.entryId);
+      let post = await fetch(`${url}comments`, {
+        ...config,
+        body: JSON.stringify({
+          comment: document.getElementById("comment").value,
+          user_id: id,
+          entry_id: entryConID,
+        }),
+      });
       let data = await post.json();
       // debugger
 
@@ -200,32 +202,27 @@ const addComment = ({ id }) => {
         comment: document.getElementById("comment").value,
         user_id: id,
       });
-      
     }
   });
 };
-
-
 
 function returnUser(user) {
   return user;
 }
 
-
-
 /// LOG IN + SIGN UP + LOG OUT VARIABLES ///
-const logInLink = document.querySelector('.login-link')
-const logInDom = document.querySelector('.login-options')
-const signupLink = logInDom.querySelector('.signup-link')
-const loginForm = logInDom.querySelector('form')
-const logoutLink = document.querySelector('.logout')
+const logInLink = document.querySelector(".login-link");
+const logInDom = document.querySelector(".login-options");
+const signupLink = logInDom.querySelector(".signup-link");
+const loginForm = logInDom.querySelector("form");
+const logoutLink = document.querySelector(".logout");
 
 /////// SIGNUP SECTION //////
 
 // let sessionStorage.getItem('user_id');
-const signUpDom = document.querySelector('.sign-up-options')
-const loggedInDom = document.querySelector('.logged-in-options')
-  loggedInDom.style.display = "none"
+const signUpDom = document.querySelector(".sign-up-options");
+const loggedInDom = document.querySelector(".logged-in-options");
+loggedInDom.style.display = "none";
 
 let signupForm = document.getElementById("signup-form");
 
@@ -237,111 +234,103 @@ signupForm.addEventListener("submit", (e) => {
     name: signupForm.InputName.value,
     username: signupForm.InputUserName.value,
     age: signupForm.InputAge.value,
-    password: signupForm.InputPassword.value
-  }
+    password: signupForm.InputPassword.value,
+  };
 
   const userObjConfig = {
     ...config,
-    body: JSON.stringify(userObj)
-  }
+    body: JSON.stringify(userObj),
+  };
 
   fetch(`${url}users`, userObjConfig)
-  .then(r=>r.json())
-  .then(newUser => {
-    // sessionStorage.getItem('user_id') = newUser.id
-    signUpDom.style.display = "none"
-    // debugger
-    // render your first entry (if you ha)
-    logIn(sessionStorage.getItem('user_id'))
-
-  }).then(saveUserSession)
-
+    .then((r) => r.json())
+    .then((newUser) => {
+      // sessionStorage.getItem('user_id') = newUser.id
+      signUpDom.style.display = "none";
+      // debugger
+      // render your first entry (if you ha)
+      logIn(sessionStorage.getItem("user_id"));
+    })
+    .then(saveUserSession);
 });
 
 //// LOG IN SECTION ///
 
-signupLink.addEventListener('click', (e)=>{
-  e.preventDefault()
-  signUpDom.style.display = ""
-  logInDom.style.display = "none"
-})
+signupLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  signUpDom.style.display = "";
+  logInDom.style.display = "none";
+});
 
-logInLink.addEventListener('click', (e)=>{
-  e.preventDefault()
-  signUpDom.style.display = "none"
-  logInDom.style.display = ""
-} )
+logInLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  signUpDom.style.display = "none";
+  logInDom.style.display = "";
+});
 
-
-loginForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   const getLoggedUser = {
-    username: loginForm['log-in-username'].value,
-    password: loginForm['log-in-password'].value
-  }
+    username: loginForm["log-in-username"].value,
+    password: loginForm["log-in-password"].value,
+  };
 
   const userConfig = {
     ...config,
-    body: JSON.stringify(getLoggedUser)
-  }
+    body: JSON.stringify(getLoggedUser),
+  };
 
-  const logInOrNot = (user) =>{
-    if (JSON.stringify(user) == "{}"){
-      console.log("incorrect password try again")
+  const logInOrNot = (user) => {
+    if (JSON.stringify(user) == "{}") {
+      console.log("incorrect password try again");
     } else {
       // console.log("correct password", user)
       // sessionStorage.getItem('user_id') = user.id
 
       //function to log in with user id argument
-      logIn(sessionStorage.getItem('user_id'))
+      logIn(sessionStorage.getItem("user_id"));
 
       // hide log in form
-      logInDom.style.display = "none"
+      logInDom.style.display = "none";
 
-
-    return user
+      return user;
     }
-  }
+  };
 
-   fetch(`${url}logs`, userConfig)
-   .then(r=>r.json())
-   .then(logInOrNot)
-   .then(saveUserSession)
-
- })
-
+  fetch(`${url}logs`, userConfig)
+    .then((r) => r.json())
+    .then(logInOrNot)
+    .then(saveUserSession);
+});
 
 ///// LOG OUT SECTION ///
 
-logoutLink.addEventListener('click', (e)=>{
+logoutLink.addEventListener("click", (e) => {
   // debugger
-  e.preventDefault()
+  e.preventDefault();
 
   // removing previously logged in user info from DOM
-  const cardbodyH5 = entryContainer.querySelector('h5')
-  const cardbodyP = entryContainer.querySelector('p')
-  cardbodyH5.innerText = ""
-  cardbodyP.innerText = ""
-  userInfoContainer.innerText = ""
-  myEntriesDiv.innerText = ""
+  const cardbodyH5 = entryContainer.querySelector("h5");
+  const cardbodyP = entryContainer.querySelector("p");
+  cardbodyH5.innerText = "";
+  cardbodyP.innerText = "";
+  userInfoContainer.innerText = "";
+  myEntriesDiv.innerText = "";
 
   // entriesLink.removeChild(myEntriesDiv)
 
-  publicLinks.innerText = ""
-  //sessionStorage.removeItem('user_id')
+  publicLinks.innerText = "";
+  sessionStorage.removeItem('user_id')
 
   // reveal main content after logging in
   // loggedInDom.textContent = ""
   // debugger
-  loggedInDom.style.display = "none"
-  logInDom.style.display = "none"
+  loggedInDom.style.display = "none";
+  logInDom.style.display = "none";
   // debugger
-  signUpDom.style.display = ""
-
-
-
-})
+  signUpDom.style.display = "";
+});
 
 // function openForm() {
 //   document.getElementById("myForm").style.display = "block";
@@ -351,102 +340,98 @@ logoutLink.addEventListener('click', (e)=>{
 //   document.getElementById("myForm").style.display = "none";
 // }
 
-
 /////// CREATE A POST SECTION!!! //////
 
 // Variables
 
-const addPostBtn = document.querySelector('.add-post')
-const entryCard = document.querySelector('#entry-container')
-const postFormDiv = document.querySelector('.myForm')
-const postForm = postFormDiv.querySelector('form')
+const addPostBtn = document.querySelector(".add-post");
+const entryCard = document.querySelector("#entry-container");
+const postFormDiv = document.querySelector(".myForm");
+const postForm = postFormDiv.querySelector("form");
 
 // event listener on add post btn to open a form for new post
-addPostBtn.addEventListener('click', (e)=>{
-  e.preventDefault()
+addPostBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-  if (addPostBtn.innerText == "Cancel"){
-    addPostBtn.innerText = "Add New Post"
-    entryCard.style.display = ""
-    postFormDiv.style.display = "none"
-    console.log("entry shown again and button should go back to normal")
+  if (addPostBtn.innerText == "Cancel") {
+    addPostBtn.innerText = "Add New Post";
+    entryCard.style.display = "";
+    postFormDiv.style.display = "none";
+    console.log("entry shown again and button should go back to normal");
   } else {
-    addPostBtn.innerText = "Cancel"
-    entryCard.style.display = "none"
-    postFormDiv.style.display = ""
-    console.log("display form here and post button name change")
+    addPostBtn.innerText = "Cancel";
+    entryCard.style.display = "none";
+    postFormDiv.style.display = "";
+    console.log("display form here and post button name change");
   }
-  
-
-})
-
+});
 
 // new entry functions to slap to DOM with event listeners
 const newEntryFunc = (newEntry) => {
   //initial render entry after submitting form
-  renderEntry(newEntry)
-  const newEntryTitle = newEntry.title
+  renderEntry(newEntry);
+  const newEntryTitle = newEntry.title;
 
   // append as p tag to MY post list
-    // if no div section in my entriesLink create one and add it
-  const newEntryTitleP = createP(newEntryTitle)
+  // if no div section in my entriesLink create one and add it
+  const newEntryTitleP = createP(newEntryTitle);
 
-  myEntriesDiv.append(newEntryTitleP)
+  myEntriesDiv.append(newEntryTitleP);
 
   //listener for new entry added to render form when clicked later
+  
   newEntryTitleP.addEventListener("click", () => {
     renderEntry(newEntry);
-  });
+  }); // generalize to function 
 
   //append as li to ALL post list
-  const otherLinksUl = publicLinks.querySelector('ul')
-  const link = createLi(newEntryTitle);
-  link.addEventListener("click", () => renderEntry(newEntry))
-  otherLinksUl.append(link)
+  if (newEntry.public) {
+    const otherLinksUl = publicLinks.querySelector("ul");
+    const link = createLi(newEntryTitle);
+    link.addEventListener("click", () => renderEntry(newEntry));
+    otherLinksUl.append(link);
+  }
 
   //hide post form and reset contents if successful
-  postForm.reset()
-  addPostBtn.innerText = "Add New Post"
-  entryCard.style.display = ""
-  postFormDiv.style.display = "none"
-  console.log("entry shown again and button should go back to normal")
-}
-
+  postForm.reset();
+  addPostBtn.innerText = "Add New Post";
+  entryCard.style.display = "";
+  postFormDiv.style.display = "none";
+  console.log("entry shown again and button should go back to normal");
+};
 
 // event listener for submitting new post
-postForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
+postForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  const postTitle = postForm['post-title'].value
-  const postDesc = postForm['post-desc'].value
-  const thisUser = sessionStorage.getItem('user_id')
-  const publicPost = postForm.public.checked
+  const postTitle = postForm["post-title"].value;
+  const postDesc = postForm["post-desc"].value;
+  const thisUser = sessionStorage.getItem("user_id");
+  const publicPost = postForm.public.checked;
 
-  
   const newPostObj = {
     title: postTitle,
     description: postDesc,
     user_id: thisUser,
-    public: publicPost
-  }
-  
+    public: publicPost,
+  };
+
   const newPostConfig = {
-   ...config,
-    body: JSON.stringify(newPostObj)
-  }
-  
+    ...config,
+    body: JSON.stringify(newPostObj),
+  };
+
   fetch(`${url}entries`, newPostConfig)
-  .then(r=>r.json())
-  .then(newEntryFunc)
+    .then((r) => r.json())
+    .then(newEntryFunc);
 
   // debugger
-})
-
+});
 
 ///// EDITING POST SECTION /////
 
 //find edit post button a-tag
-const editPostBtn = document.querySelector('.edit-post')
+const editPostBtn = document.querySelector(".edit-post");
 
 //event listener for btn
 // editPostBtn.addEventListener('click', (e)=>{
@@ -454,18 +439,12 @@ const editPostBtn = document.querySelector('.edit-post')
 //   console.log("edit post button clicked")
 // })
 
-  //making sure this post is mine (post title comes with entryId and userId) (if currentUserId == userId)
-
-
+//making sure this post is mine (post title comes with entryId and userId) (if currentUserId == userId)
 
 /////// END ///////
 
-
-
-
-
 //////// DELETING POST SECTION ///////
-const deletePostBtn = document.querySelector('.delete-post')
+const deletePostBtn = document.querySelector(".delete-post");
 
 // //event listener for btn
 // deletePostBtn.addEventListener('click', (e)=>{
@@ -473,37 +452,34 @@ const deletePostBtn = document.querySelector('.delete-post')
 //   console.log("delete post button clicked")
 // })
 
-
-function saveUserSession({id}) {
-       sessionStorage.setItem('user_id', id)
+function saveUserSession({ id }) {
+  sessionStorage.setItem("user_id", id);
 }
 //// END //////
 
-
 function logIn(id) {
-    // reveal main content after logging in
-    loggedInDom.style.display = "" //visible
-    console.log(id)
-    fetch(`${url}/users/${id}`)
-    .then(r=>r.json())
-    .then(data=>{
+  // reveal main content after logging in
+  loggedInDom.style.display = ""; //visible
+  console.log(id);
+  fetch(`${url}/users/${id}`)
+    .then((r) => r.json())
+    .then((data) => {
       // commentForm.removeEventListener('submit', ()=>{})
       renderUser(data);
       renderEntriesLinks(data);
       fetchAllEntries(appendLinksToPage);
       addComment(data);
-    })
-  }
-
-
-function init() {
-    let id = sessionStorage.getItem('user_id')
-    if(id) {
-        // signupDom.style.display = 'none'
-        logInDom.style.display = "none"
-        signUpDom.style.display = "none"
-        logIn(id)
-    }
+    });
 }
 
-init()
+function init() {
+  let id = sessionStorage.getItem("user_id");
+  if (id) {
+    // signupDom.style.display = 'none'
+    logInDom.style.display = "none";
+    signUpDom.style.display = "none";
+    logIn(id);
+  }
+}
+
+init();
